287c89b21bc603ebb417707377685e9614fd6204
unlimited <fengde1992@gmail.com>
2014-04-09 09:29:17 +0800

    store the id not the GraphicNode still Memory Limit Exceed

--- src/com/unlimited/poj/poj1753/Main.java
+++ src/com/unlimited/poj/poj1753/Main.java
@@ -10,8 +10,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
         Scanner in = new Scanner(System.in);
         int i = 0;
         List<String> strings = new ArrayList<String>();
@@ -21,7 +19,11 @@ public class Main {
         }
         Graphics g = new Graphics(strings);
 
+        long start = System.currentTimeMillis();
         GraphicsNode node = BFS(g, new Visitor());
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
         if (node != null) {
             System.out.println(node.length);
         } else {
@@ -36,19 +38,20 @@ public class Main {
      * @return the node we found, null if not found
      */
     public static GraphicsNode BFS(Graphics g, Visitor v) {
-        Queue<GraphicsNode> queue = new LinkedList<GraphicsNode>();
-        queue.offer(g.root);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(g.root.id);
 
         while (!queue.isEmpty()) {
-            GraphicsNode node = queue.poll();
-            if (v.visit(node)) {
+            GraphicsNode node = g.getNode(queue.poll());
+            if (node.accept(v)) {
                 return node;
             }
 
-            List<GraphicsNode> nodes = node.getChildren();
-            for (GraphicsNode n : nodes) {
+            List<Integer> nodes = node.getChildren();
+            for (int _id : nodes) {
+                GraphicsNode n = g.getNode(_id);
                 if (!n.visited) {
-                    queue.offer(n);
+                    queue.offer(_id);
                 }
             }
         }
@@ -59,27 +62,26 @@ public class Main {
 
 class Visitor {
     public boolean visit(GraphicsNode g) {
-        if (g.visited) {
-            return (g.id == 0xFFFF || g.id == 0);
-        } else {
-            g.visited = true;
-            return false;
-        }
+        return (g.id == 0xFFFF || g.id == 0);
     }
 }
 
 class Graphics {
-    public Map<Integer, GraphicsNode> exists = new HashMap<Integer, GraphicsNode>();
+    public Map<Integer, Integer> visited = new HashMap<Integer, Integer>();
+    public Map<Integer, Integer> newcome = new HashMap<Integer, Integer>();
     public GraphicsNode root;
 
     public GraphicsNode getNode(int id) {
-        GraphicsNode node = exists.get(id);
-        if (node == null) {
-            node = new GraphicsNode(id, this);
-            exists.put(id, node);
+        root.id = id;
+        if (visited.get(id) != null) {
+            root.length = visited.get(id);
+            root.visited = true;
+        } else {
+            root.length = newcome.get(id);
+            root.visited = false;
         }
 
-        return node;
+        return root;
     }
 
     public Graphics(List<String> strings) {
@@ -94,8 +96,10 @@ class Graphics {
             j = 0;
         }
 
-        root = getNode(id);
+        root = new GraphicsNode(id, this);
         root.length = 0;
+
+        newcome.put(id, 0);
     }
 }
 
@@ -104,7 +108,7 @@ class GraphicsNode {
     public int length;
     public Graphics owner;
     public boolean visited;
-    private List<GraphicsNode> children;
+    private List<Integer> children;
     private static int TRANSFER[] = {
             0xC800, 0xE400, 0x7200, 0x3100,
             0x8C80, 0x4E40, 0x2720, 0x1310,
@@ -118,18 +122,29 @@ class GraphicsNode {
         this.owner = owner;
     }
 
-    public GraphicsNode flip(int transfer, int id) {
-        return owner.getNode(id ^ transfer);
+    public boolean accept(Visitor v) {
+        if (owner.newcome.get(id) != null) {
+            owner.visited.put(id, owner.newcome.get(id));
+            owner.newcome.remove(id);
+            return v.visit(this);
+        }
+
+        return false;
     }
 
-    public List<GraphicsNode> getChildren() {
+    public List<Integer> getChildren() {
         if (children == null) {
-            children = new ArrayList<GraphicsNode>();
-            for (int i : TRANSFER) {
-                GraphicsNode node = this.flip(i, id);
-                node.length = this.length + 1;
-                children.add(node);
+            children = new ArrayList<Integer>();
+        }
+
+        children.clear();
+        int _id;
+        for (int i : TRANSFER) {
+            _id = id ^ i;
+            if (owner.visited.get(_id) == null || owner.newcome.get(_id) == null) {
+                owner.newcome.put(_id, length + 1);
             }
+            children.add(_id);
         }
 
         return children;
